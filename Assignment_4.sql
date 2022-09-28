5c9298a010418e8eb28c38ccafc485b424a1faf3
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2022-09-29 00:12:06 +0530

    Update Assignment_4.sql

--- Assignment_4.sql
+++ Assignment_4.sql
@@ -19,31 +19,31 @@ SQL> create table circle_area(radius int,area number);
 Table created.
 
 SQL> DECLARE
-  2  r int;
-  3  a number;
-  4  BEGIN
-  5  FOR r in 5..9 LOOP
-  6  a:= 3.14 * r * r;
-  7  INSERT INTO circle_area VALUES(r,a);
-  8  dbms_output.Put_line('Area of circle is  = ' || a);
-  9  END LOOP;
- 10  END;
- 11  /
+    r int;
+    a number;
+    BEGIN
+    FOR r in 5..9 LOOP
+    a:= 3.14 * r * r;
+    INSERT INTO circle_area VALUES(r,a);
+    dbms_output.Put_line('Area of circle is  = ' || a);
+    END LOOP;
+    END;
+    /
 
 PL/SQL procedure successfully completed.
 
 SQL> SET SERVEROUTPUT ON
 SQL> DECLARE
-  2  r int;
-  3  a number;
-  4  BEGIN
-  5  FOR r in 5..9 LOOP
-  6  a:= 3.14 * r * r;
-  7  INSERT INTO circle_area VALUES(r,a);
-  8  dbms_output.Put_line('Area of circle is  = ' || a);
-  9  END LOOP;
- 10  END;
- 11  /
+    r int;
+    a number;
+    BEGIN
+    FOR r in 5..9 LOOP
+    a:= 3.14 * r * r;
+    INSERT INTO circle_area VALUES(r,a);
+    dbms_output.Put_line('Area of circle is  = ' || a);
+    END LOOP;
+    END;
+    /
 Area of circle is  = 78.5
 Area of circle is  = 113.04
 Area of circle is  = 153.86
